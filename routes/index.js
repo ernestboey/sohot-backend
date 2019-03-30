ab161727265d20fd39b72b4dd5640ed707f7cc2e
ernestboey <ernestboeywz@gmail.com>
2019-03-30 16:40:36 +0800

    Add retrieve user info endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,39 @@
 const express = require('express');
 
+const User = require('../models/User');
+const AddOn = require('../models/AddOn');
+
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.post('/info/:phoneNumber', (req, res, next) => {
+  User.findOne({ number: req.params.phoneNumber })
+    .populate('addOns')
+    .then((user) => {
+      console.log(user);
+      res.json({ success: true, user });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({ success: false, message: 'Error' });
+    });
+});
+
+router.post('/add/:addOnId', (req, res, next) => {
+  AddOn.find({})
+    .then((addOns) => {})
+    .catch((err) => {
+      console.error(err);
+      res.json({ success: false, message: 'Error' });
+    });
+});
+
+router.post('/getAllOns', (req, res, next) => {
+  AddOn.find({})
+    .then((addOns) => {})
+    .catch((err) => {
+      console.error(err);
+      res.json({ success: false, message: 'Error' });
+    });
 });
 
 module.exports = router;
